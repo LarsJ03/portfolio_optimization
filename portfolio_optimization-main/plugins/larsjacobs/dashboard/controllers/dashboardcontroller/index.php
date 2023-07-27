d737be4390e424a279acdbf0f517d8a7020398e9
Mark Jacobs <mark.jacobs120669@gmail.com>
2023-07-27 17:33:31 +0200

    update

--- portfolio_optimization-main/plugins/larsjacobs/dashboard/controllers/dashboardcontroller/index.php
+++
@@ -1,4 +0,0 @@
-<div style="width: 80%; margin: 0 auto;">
-    <script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script>
-    <tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/industrialanalyticalreporting/views/portfolio_optimization/Dashboard1' width='100%' height='840' hide-tabs toolbar='bottom' ></tableau-viz>
-</div>
--- portfolio_optimization-main/plugins/larsjacobs/dashboard/lang/en/lang.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'plugin' => [
-        'name' => 'Dashboard',
-        'description' => ''
-    ]
-];
